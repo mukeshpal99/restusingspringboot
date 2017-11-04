5981754da2f35e2a5ca83bad4e5b91b82bf1931e
mukeshpalad <mukesh.pal@appdirect.com>
2017-11-05 00:30:15 +0530

    learned i18n enablement for web service

--- src/main/java/com/learning/rest/learningrest/helloworld/HelloWorldController.java
+++ src/main/java/com/learning/rest/learningrest/helloworld/HelloWorldController.java
@@ -1,11 +1,19 @@
 package com.learning.rest.learningrest.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	MessageSource messageSource;
 	
 	@GetMapping("/hello-world")
 	public String helloWorld(){
@@ -21,9 +29,16 @@ public class HelloWorldController {
 	
 	
 	// return a hello world bean
-		@GetMapping("/hello-world-bean/{name}")
-		public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name){
-			return new HelloWorldBean(String.format("Hello %s", name), "Monday");
-		}
+	@GetMapping("/hello-world-bean/{name}")
+	public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name){
+		return new HelloWorldBean(String.format("Hello %s", name), "Monday");
+	}
+	
+	@GetMapping("/hello-world/i18n")
+	public String helloWorldI18n(@RequestHeader(name="Accept-Language", required=false) Locale locale){
+		return messageSource.getMessage("good.morning.message", null, locale);
+	}
+	
+	
 
 }
